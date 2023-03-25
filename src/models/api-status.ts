a125d641b5f9f4776466b9df85ea0cea2aa1a356
`thais` <`tjlannes@gmail.com`>
2023-03-25 16:51:57 +0100

    create api status file

---
+++ src/models/api-status.ts
@@ -0,0 +1,12 @@
+export enum APIStatus {
+  LOADING = 'loading',
+  ERROR = 'failed',
+  IDLE = 'idle',
+}
+
+export enum AuthStatus {
+  LOADING = 'loading',
+  ERROR = 'failed',
+  SUCCESS = 'idle',
+  NOT_USED = 'notUsed',
+}
