6e1643e23a86a99f10fad01987333998553cf4f9
`thais` <`tjlannes@gmail.com`>
2023-03-27 22:21:53 +0200

    create router of create tour

--- src/app/router/router.tsx
+++ src/app/router/router.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import HomePage from '../../pages/Home/HomePage';
 import LoginPage from '../../pages/Login/LoginPage';
 import NotFound from '../../pages/NotFound/NotFound';
+import CreateTour from '../../pages/CreateTour/CreateTour';
 import MainLayout from '../layout/MainLayout';
 
 export const router = createBrowserRouter([
@@ -23,6 +24,10 @@ export const router = createBrowserRouter([
       {
         path: '',
         element: <HomePage />,
+      },
+      {
+        path: 'create-tour',
+        element: <CreateTour />,
       },
     ],
   },
