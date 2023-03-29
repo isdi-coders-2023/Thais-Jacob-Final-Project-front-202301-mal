c912e92663ba9f48060a34ed06dc54d40f00ebae
`thais` <`tjlannes@gmail.com`>
2023-03-29 12:51:26 +0200

    update tour model and tour api

--- src/features/tour/tour-list-api.tsx
+++ src/features/tour/tour-list-api.tsx
@@ -1,3 +1,5 @@
+import { ApiResponse, TourDetailInterface } from '../../models/tour-model';
+
 export const getToursList = async () => {
   const response = await fetch(
     `${process.env.REACT_APP_API_URL}/api/v1/tours`,
@@ -8,6 +10,30 @@ export const getToursList = async () => {
       },
     },
   );
+  if (!response.ok) {
+    throw new Error('It looks like your data is not correct...');
+  }
 
   return response;
+};
+
+export const getTourById = async (
+  id: string,
+): Promise<ApiResponse<TourDetailInterface>> => {
+  const response = await fetch(
+    `${process.env.REACT_APP_API_URL}/api/v1/tours/${id}`,
+    {
+      headers: {
+        Authorization: `Bearer ${sessionStorage.getItem('accessToken')}`,
+        'Content-Type': 'application/json',
+      },
+    },
+  );
+  const tourResponse: ApiResponse<TourDetailInterface> = await response.json();
+
+  if (!response.ok) {
+    throw new Error('Oops, it seems it is not possible to see this tour 🙁');
+  }
+
+  return tourResponse as TourDetailInterface;
 };
