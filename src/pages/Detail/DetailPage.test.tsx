57d3c8e0fccf1bdfe32c71cbf7a3fba9d923a7e3
`thais` <`tjlannes@gmail.com`>
2023-03-29 14:09:13 +0200

    add test cases in detail page

--- src/pages/Detail/DetailPage.test.tsx
+++ src/pages/Detail/DetailPage.test.tsx
@@ -1,10 +1,15 @@
 import { screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
+import { server } from '../../mocks/server';
 import { renderWithProviders } from '../../mocks/test-utils';
 import DetailPage from './DetailPage';
 
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
 describe('Given a detail page', () => {
-  test('When there is a title, then render it on the page', () => {
+  test('When it is loading, then render a loading gif on the page', () => {
     renderWithProviders(
       <MemoryRouter>
         <DetailPage />
@@ -12,5 +17,15 @@ describe('Given a detail page', () => {
     );
     const loading = screen.getByAltText('Waiting for data loading');
     expect(loading).toBeInTheDocument();
+  });
+
+  test('When tour details are selected, this is rendered on the page.', () => {
+    renderWithProviders(
+      <MemoryRouter>
+        <DetailPage />
+      </MemoryRouter>,
+    );
+    const image = screen.getByRole('img');
+    expect(image).toBeInTheDocument();
   });
 });
