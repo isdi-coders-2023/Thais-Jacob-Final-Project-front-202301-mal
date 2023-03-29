f280a8ca6d5702d7a7177014cdbdaf1f72aa0d28
`thais` <`tjlannes@gmail.com`>
2023-03-29 12:42:08 +0200

    creation of a test environment

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,4 +1,10 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import {
+  configureStore,
+  ThunkAction,
+  Action,
+  combineReducers,
+  PreloadedState,
+} from '@reduxjs/toolkit';
 import authReducer from '../features/auth/auth-slice';
 import toursReducer from '../features/tour/tour-list-slice';
 
@@ -9,6 +15,19 @@ export const store = configureStore({
   },
 });
 
+const rootReducer = combineReducers({
+  auth: authReducer,
+  tours: toursReducer,
+});
+
+export const setupStore = (preloadedState?: PreloadedState<RootState>) => {
+  return configureStore({
+    reducer: rootReducer,
+    preloadedState,
+  });
+};
+
+export type AppStore = ReturnType<typeof setupStore>;
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppThunk<ReturnType = void> = ThunkAction<
---
+++ src/mocks/test-utils.tsx
@@ -0,0 +1,49 @@
+import { PreloadedState } from '@reduxjs/toolkit';
+import { render, RenderOptions } from '@testing-library/react';
+import { PropsWithChildren } from 'react';
+import { Provider } from 'react-redux';
+import { AppStore, RootState, setupStore } from '../app/store';
+import { APIStatus, TourStatus } from '../models/api-status';
+
+interface ExtendedRenderOptions extends Omit<RenderOptions, 'queries'> {
+  preloadedState?: PreloadedState<RootState>;
+  store?: AppStore;
+}
+
+export const renderWithProviders = (
+  ui: React.ReactElement,
+  {
+    preloadedState = {
+      auth: {
+        status: 'idle',
+        loginStatus: 'idle',
+        loginMsg: '',
+      },
+      tours: {
+        tours: [],
+        tour: {
+          _id: '',
+          title: '',
+          description: '',
+          video: '',
+          image: '',
+          meetingPoint: '',
+          price: 0,
+          date: new Date(),
+        },
+        status: APIStatus.IDLE,
+        responseMsg: '',
+        createStatus: TourStatus.NOT_USED,
+      },
+    },
+
+    store = setupStore(preloadedState),
+    ...renderOptions
+  }: ExtendedRenderOptions = {},
+) => {
+  const Wrapper = ({ children }: PropsWithChildren<{}>): JSX.Element => {
+    return <Provider store={store}>{children}</Provider>;
+  };
+
+  return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) };
+};
